7967e063fbfa7674c64ea57f59c93a408d279eeb
kacper.sledz <casper.sledx@gmail.com>
2019-11-15 23:56:33 +0100

    keyboard manager concept

--- src/app/components/board/board.component.ts
+++ src/app/components/board/board.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Store, select } from '@ngrx/store';
 import { IAppState } from 'src/app/store/states/app.state';
 import { NewGame } from 'src/app/store/actions/game.action';
@@ -10,18 +10,46 @@ export interface BoardCell {
   selected: boolean;
 }
 
+class KeyboardManager {
+  constructor(private _store: Store<IAppState>) {
+    this.keyupHandler = this.keyupHandler.bind(this);
+  }
+
+  keyupHandler(e: KeyboardEvent) {
+    const keyValue = parseInt(e.key, 10);
+    if (!isNaN(keyValue) && keyValue >= 1 && keyValue <= 9) {
+      // TODO dispatch key click
+    }
+  }
+
+  subscribe() {
+    document.addEventListener('keyup', this.keyupHandler);
+  }
+  unsubscribe() {
+    document.removeEventListener('keyup', this.keyupHandler);
+  }
+}
+
 @Component({
   selector: 'app-board',
   templateUrl: './board.component.html',
   styleUrls: ['./board.component.scss']
 })
-export class BoardComponent implements OnInit {
+export class BoardComponent implements OnInit, OnDestroy {
   board$ = this._store.pipe(select(selectBoard));
+  private _keyboardManager: KeyboardManager;
 
-  constructor(private _store: Store<IAppState>) {}
+  constructor(private _store: Store<IAppState>) {
+    this._keyboardManager = new KeyboardManager(_store);
+  }
 
   ngOnInit() {
     this._store.dispatch(new NewGame());
+    this._keyboardManager.subscribe();
+  }
+
+  ngOnDestroy(): void {
+    this._keyboardManager.unsubscribe();
   }
 
   trackItem(_index: number, item: BoardCell) {
