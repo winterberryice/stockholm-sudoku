17f1e7f59cb1c6d722dada4d997731012fc6ac6c
kacper.sledz <casper.sledx@gmail.com>
2019-11-24 12:39:15 +0100

    board controls action

--- src/app/components/board-controls/board-controls.component.ts
+++ src/app/components/board-controls/board-controls.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { IAppState } from 'src/app/store/states/app.state';
+import {
+  FillCellValue,
+  ClearUserCellValue
+} from 'src/app/store/actions/game.action';
 
 @Component({
   selector: 'app-board-controls',
@@ -6,10 +12,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./board-controls.component.scss']
 })
 export class BoardControlsComponent implements OnInit {
+  controlsModel: { value: number }[] = [];
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private _store: Store<IAppState>) {
+    for (let i = 1; i <= 10; i++) {
+      this.controlsModel.push({ value: i % 10 });
+    }
   }
 
+  ngOnInit() {}
+
+  trackByFn(index: any, _item: any) {
+    return index;
+  }
+
+  onClick(value: number) {
+    if (value !== 0) {
+      this._store.dispatch(new FillCellValue({ value }));
+    } else {
+      this._store.dispatch(new ClearUserCellValue());
+    }
+  }
 }
